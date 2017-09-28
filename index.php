dc5950fe1fb6c0a6a5107f39c6aa62d5db3bc535
taslimR <hridoy.bubd@gmail.com>
2017-09-28 21:37:35 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,477 @@
+﻿
+<!DOCTYPE html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta name="keywords" content="admin template, admin dashboard, inbox templte, calendar template, form validation">
+<meta name="author" content="DazeinCreative">
+<meta name="description" content="ORB - Powerfull and Massive Admin Dashboard Template with tonns of useful features">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Dashboard Showcase</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<script type="text/javascript" src="js/vendors/modernizr/modernizr.custom.js"></script>
+</head>
+
+<body>
+<!--<div class="colorful-page-wrapper" style="position:fixed; z-index:100000; top:0; left:0;">
+  <div class="center-block">
+    <div class="login-block" style="float:left; width:100%; padding:0;">
+      <form action="index.html" id="login-form" class="WHYALOG-form" style="float:left; width:100%;">
+        <header style="float:left; width:100%; margin:0;">
+          <div class="image-block"><img src="images/logo.png" alt="User" /></div>
+        </header>
+        <fieldset style="float:left; width:100%; padding:15px 20px;">
+          <section>
+            <div class="row">
+              <div class="col-sm-12">
+              	<div class="form-group">
+                  <input class="form-control" id="username" type="text" name="username" placeholder="Username..">
+              	</div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div class="row">
+              <div class="col-sm-12">
+              	<div class="form-group">
+                  <input class="form-control" id="password" type="password" name="password" placeholder="Password..">
+              	</div>
+              </div>
+            </div>
+          </section>
+          
+        </fieldset>
+        <footer style="width:100%; float:left; margin:0; padding:10px; text-align:center;">
+          <button type="submit" id="submit" class="btn btn-default">Log in</button>
+        </footer>
+      </form>
+    </div>
+  </div>
+</div>-->
+
+<!--Smooth Scroll-->
+<div class="smooth-overflow"> 
+  <!--Navigation-->
+  <nav class="main-header clearfix" style="background-color: #f58426;" role="navigation"> <a class="navbar-brand" href="index.html"><img style="height:30px;" src="images/brand.png" alt=""></a> 
+    
+    <!--Navigation Itself-->
+  </nav>
+  
+  <!--/Navigation--> 
+  
+  <!--MainWrapper-->
+  <div class="main-wrap"> 
+    
+    
+    
+    <div class="content-wrapper main-content-toggle-left"> 
+      <!--Content Wrapper--><!--Horisontal Dropdown-->
+      
+      
+      
+      <div class="page-header">
+          <h1>Dashboard<small>Showcase</small></h1>
+        </div>
+      
+      <!-- Widget Row Start grid -->
+      <div class="row" id="powerwidgets">
+      
+      	<div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget powerwidget-as-portlet-cold-grey" id="livestats" data-widget-editbutton="false">
+            <header>
+              <h2>Live Stats<small>Moving Anarchy</small></h2>
+            </header>
+            <div class="inner-spacer">
+              <div class="flotchart-container bg-cold-grey">
+                <div class="inner-stuff">
+                  <div class="desc pull-left hidden-xs">
+                    <h4 class="margin-0px margin-bottom-10px"><span class="text-blue">Inbound</span> <i class="fa fa-dot-circle-o"></i> <span class="text-green">Outbound</span> Traffic</h4>
+                    <span class="label bg-marine">Uptime: 234:45:16</span></div>
+                  <div class="btn-group btn-group-sm pull-right">
+                    <button class="btn btn-default active" id="start-stop-flot">Pause</button>
+                    <button class="btn btn-default" id="accelerate-flot">Update Rate: 1</button>
+                  </div>
+                </div>
+                <div id="placeholder7" class="flotchart-placeholder margin-negative-top-20px"></div>
+              </div>
+              <div class="row margin-negative-top-10px"> 
+                <!--Row-->
+                <ul class="tiny-stats margin-negative-top-20px">
+                  <li class="col-md-3 col-sm-3 col-xs-6">
+                    <div id="g1"></div>
+                    <div class="btn-group btn-group-sm">
+                      <div class="btn btn-default">Manage</div>
+                      <div class="btn btn-info">More</div>
+                    </div>
+                  </li>
+                  <li class="col-md-3 col-sm-3 col-xs-6">
+                    <div id="g2"></div>
+                    <div class="btn-group btn-group-sm">
+                      <div class="btn btn-info">Clear</div>
+                      <div class="btn btn-default">Optimize</div>
+                    </div>
+                  </li>
+                  <li class="col-md-3 col-sm-3 col-xs-6">
+                    <div id="g3"></div>
+                    <div class="btn-group btn-group-sm">
+                      <div class="btn btn-info">Clear</div>
+                      <div class="btn btn-default">Cache</div>
+                    </div>
+                  </li>
+                  <li class="col-md-3 col-sm-3 col-xs-6">
+                    <div id="g4"></div>
+                    <div class="btn-group btn-group-sm">
+                      <div class="btn btn-info">Queries</div>
+                      <div class="btn btn-default">More</div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <!--/Row--> 
+              
+            </div>
+          </div>
+          <!-- End Widget --> 
+          
+          
+          
+        </div>
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/demo/index.php/welcome/dashboard" style="width:100%; height:2000px; margin-top:-110px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        
+        
+        
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>PMRI Step 3</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/pmri/dpp_s3/" style="width:100%; height:2864px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>PMRI Step 4</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/pmri/dpp_s4/" style="width:100%; height:1113px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        
+        
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>NTY-Slider</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/nyt-slider/v5/" style="width:100%; height:600px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>Valuation</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/valuation/v4/" style="width:100%; height:800px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>Continent Globe</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/globe/v4/" style="width:100%; height:630px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        <div class="col-md-12"> <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>Quality drivers</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/quality_drivers/v7/iframe/index.html" style="width:100%; height:820px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        <div class="col-md-12"> 
+          <!-- New widget -->
+          <div class="powerwidget" data-widget-editbutton="false">
+            <header>
+              <h2>Multi-series line chart interactive</h2>
+            </header>
+            <div class="inner-spacer">
+            <div class="row" style="overflow:hidden;">
+              <iframe src="http://lordier.fr/modified-project/d3-object/multi-series-line-chart/v4/" style="width:100%; height:620px;" frameborder="0"></iframe>
+              </div>
+            </div>
+          </div>
+          <!-- /New widget --> 
+        </div>
+        
+        
+        
+      </div>
+      <!-- /Widgets Row End Grid--> 
+    </div>
+    <!-- / Content Wrapper --> 
+  </div>
+  <!--/MainWrapper--> 
+</div>
+<!--/Smooth Scroll--> 
+
+<!-- scroll top -->
+<div class="scroll-top-wrapper hidden-xs"> <i class="fa fa-angle-up"></i> </div>
+<!-- /scroll top --> 
+
+<!--Modals--> 
+
+<!--Power Widgets Modal-->
+<div class="modal" id="delete-widget">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <i class="fa fa-lock"></i> </div>
+      <div class="modal-body text-center">
+        <p>Are you sure to delete this widget?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal" id="trigger-deletewidget-reset">Cancel</button>
+        <button type="button" class="btn btn-primary" id="trigger-deletewidget">Delete</button>
+      </div>
+    </div>
+    <!-- /.modal-content --> 
+  </div>
+  <!-- /.modal-dialog --> 
+</div>
+<!-- /.modal --> 
+
+<!--Sign Out Dialog Modal-->
+<div class="modal" id="signout">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <i class="fa fa-lock"></i> </div>
+      <div class="modal-body text-center">Are You Sure Want To Sign Out?</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="yesigo">Ok</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+    <!-- /.modal-content --> 
+  </div>
+  <!-- /.modal-dialog --> 
+</div>
+<!-- /.modal --> 
+
+<!--Lock Screen Dialog Modal-->
+<div class="modal" id="lockscreen">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <i class="fa fa-lock"></i> </div>
+      <div class="modal-body text-center">Are You Sure Want To Lock Screen?</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="yesilock">Ok</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+    <!-- /.modal-content --> 
+  </div>
+  <!-- /.modal-dialog --> 
+</div>
+<!-- /.modal --> 
+
+<!--Scripts--> 
+<!--JQuery--> 
+<script type="text/javascript" src="js/vendors/jquery/jquery.min.js"></script> 
+<script type="text/javascript" src="js/vendors/jquery/jquery-ui.min.js"></script> 
+
+<!--EasyPieChart--> 
+<script type="text/javascript" src="js/vendors/easing/jquery.easing.1.3.min.js"></script> 
+<script type="text/javascript" src="js/vendors/easypie/jquery.easypiechart.min.js"></script> 
+
+<!--Fullscreen--> 
+<script type="text/javascript" src="js/vendors/fullscreen/screenfull.min.js"></script> 
+
+<!--NanoScroller--> 
+<script type="text/javascript" src="js/vendors/nanoscroller/jquery.nanoscroller.min.js"></script> 
+
+<!--Sparkline--> 
+<script type="text/javascript" src="js/vendors/sparkline/jquery.sparkline.min.js"></script> 
+
+<!--Horizontal Dropdown--> 
+<script type="text/javascript" src="js/vendors/horisontal/cbpHorizontalSlideOutMenu.js"></script> 
+<script type="text/javascript" src="js/vendors/classie/classie.js"></script> 
+
+<!--PowerWidgets--> 
+<script type="text/javascript" src="js/vendors/powerwidgets/powerwidgets.min.js"></script> 
+
+<!--Morris Chart--> 
+<script type="text/javascript" src="js/vendors/raphael/raphael-min.js"></script> 
+
+<!--Just Gauge--> 
+<script type="text/javascript" src="js/vendors/just-gauge/justgage.1.0.1.min.js"></script> 
+
+<!--Just Gauge JS will used only here, because it is experimental--> 
+<script>
+      var g1, g2, g3;
+      
+      window.onload = function(){
+        var g1 = new JustGage({
+          id: "g1", 
+          value: getRandomInt(350, 980), 
+          min: 350,
+          max: 980,
+		  title: "CPU Load",
+		  levelColors: ["#5bc0de"],
+		  titleFontColor:'#fff',
+		  gaugeColor:'#33383d',
+		  valueFontColor:"#f0f0ed",
+		  showInnerShadow: false  
+        });
+        
+        var g2 = new JustGage({
+          id: "g2", 
+          value: getRandomInt(350, 980), 
+          min: 50,
+          max: 100,
+		  gaugeColor:'#33383d',
+		  title: "RAM Utilization",
+		  levelColors: ["#5bc0de"],
+		  titleFontColor:'#fff',
+		  valueFontColor:"#f0f0ed",
+ 		  showInnerShadow: false  
+        });
+        
+        var g3 = new JustGage({
+          id: "g3", 
+          value: getRandomInt(50, 100), 
+          min: 50,
+          max: 100,
+		  title: "Disc Usage",
+		  titleFontColor:'#fff',
+		  gaugeColor:'#33383d',
+		  levelColors: ["#5bc0de"],
+		  valueFontColor:"#f0f0ed",
+ 		  showInnerShadow: false
+        });
+		
+		var g4 = new JustGage({
+          id: "g4", 
+          value: getRandomInt(350, 980), 
+          min: 350,
+          max: 980,
+		  gaugeColor:'#33383d',
+		  titleFontColor:'#fff',
+		  levelColors: ["#5bc0de"],
+		   valueFontColor:"#f0f0ed",
+		  title:"DB Queries",
+		  showInnerShadow: false
+        });
+        
+
+      
+        setInterval(function() {
+          g1.refresh(getRandomInt(350, 980));
+          g2.refresh(getRandomInt(0, 49));          
+          g3.refresh(getRandomInt(50, 100));
+		            g4.refresh(getRandomInt(350, 980));
+        }, 2500);
+      };
+    </script>
+
+<!--FlotChart--> 
+<script type="text/javascript" src="js/vendors/flotchart/jquery.flot.min.js"></script> 
+<script type="text/javascript" src="js/vendors/flotchart/jquery.flot.resize.min.js"></script> 
+<script type="text/javascript" src="js/vendors/flotchart/jquery.flot.axislabels.js"></script> 
+<script type="text/javascript" src="js/vendors/flotchart/jquery.flot-tooltip.js"></script> 
+
+<!--Chart.js--> 
+<script type="text/javascript" src="js/vendors/chartjs/chart.min.js"></script> 
+
+<!--Calendar--> 
+<script type="text/javascript" src="js/vendors/fullcalendar/fullcalendar.min.js"></script> 
+<script type="text/javascript" src="js/vendors/fullcalendar/gcal.js"></script> 
+
+<!--Bootstrap--> 
+<script type="text/javascript" src="js/vendors/bootstrap/bootstrap.min.js"></script> 
+
+<!--Vector Map--> 
+<script type="text/javascript" src="js/vendors/vector-map/jquery.vmap.min.js"></script> 
+<script type="text/javascript" src="js/vendors/vector-map/jquery.vmap.sampledata.js"></script> 
+<script type="text/javascript" src="js/vendors/vector-map/jquery.vmap.world.js"></script> 
+
+<!--ToDo--> 
+<script type="text/javascript" src="js/vendors/todos/todos.js"></script> 
+
+<!--SkyCons--> 
+<script type="text/javascript" src="js/vendors/skycons/skycons.js"></script> 
+<script>
+      var icons = new Skycons({"color": "#fff"}),
+          list  = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ],
+          i;
+
+      for(i = list.length; i--; )
+        icons.set(list[i], list[i]);
+
+      icons.play();
+    </script> 
+
+<!--Main App--> 
+<script type="text/javascript" src="js/scripts.js"></script>
+<script type="text/javascript">
+	$('#submit').on('click', function(){
+		sessionStorage.setItem('user', $('#username').val());
+		sessionStorage.setItem('pass', $('#password').val());
+	});
+</script> 
+
+<!--/Scripts-->
+
+</body>
+</html>
